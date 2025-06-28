bf926732c21c5953c076a051505a47ef77c4b8a8
beatriz <designerbrm@gmail.com>
2025-06-28 08:30:03 +0200

    Nuevos cambios

--- temario.js
+++ temario.js
@@ -13,42 +13,6 @@ document.addEventListener('click', (e) => {
 });
 
 
-
-// Función para mostrar el banner si no hay una cookie de aceptación
-window.onload = function () {
-    if (!getCookie("cookiesAccepted")) {
-        document.getElementById("cookieBanner").style.display = "block";
-    }
-};
-
-// Función para aceptar las cookies y ocultar el banner
-function acceptCookies() {
-    setCookie("cookiesAccepted", "true", 365); // Guarda la aceptación por 1 año
-    document.getElementById("cookieBanner").style.display = "none";
-}
-
-// Función para crear una cookie
-function setCookie(name, value, days) {
-    let date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-// Función para leer una cookie
-function getCookie(name) {
-    let nameEQ = name + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let cookieArray = decodedCookie.split(';');
-    for (let i = 0; i < cookieArray.length; i++) {
-        let cookie = cookieArray[i].trim();
-        if (cookie.indexOf(nameEQ) === 0) {
-            return cookie.substring(nameEQ.length, cookie.length);
-        }
-    }
-    return null;
-}
-
 // Datos de los módulos
 const modulesData = {
     1: {
